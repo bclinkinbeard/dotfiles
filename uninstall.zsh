cccdf85db3ba632829290f9c1a48a5ab0298d26c
Kevin Suttle <kevin@kevinsuttle.com>
2013-11-07 19:22:27 -0500

    Initial commit of copy from @kevinSuttle

---
+++ uninstall.zsh
@@ -0,0 +1,13 @@
+rm $HOME/.bashrc
+rm $HOME/.curlrc
+rm $HOME/.rbenv/default-gems
+rm $HOME/.gemrc
+rm $HOME/.git*
+rm $HOME/.gvim*
+rm $HOME/.hg*
+rm $HOME/.hushlogin
+rm $HOME/.inputrc
+rm $HOME/.screenrc
+rm $HOME/.tmux*
+rm -dRf $HOME/.vim*
+rm $HOME/.zshrc
